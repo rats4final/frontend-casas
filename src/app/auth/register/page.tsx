d7fe739fa6728b314ff4b8954504fcdac0d5c037
rats4final <rats4final@gmail.com>
2024-03-25 18:40:40 -0400

    added register functionality

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,50 +1,126 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import api from "@/lib/api";
+import { setLogInCookie } from "@/lib/authCookies";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const registerFormSchema = z.object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string(),
-    password_confirmation: z.string()
-})
+  name: z.string().min(1),
+  email: z.string().email().min(1),
+  password: z.string().optional(),
+  password_confirmation: z.string().optional(),
+}).refine(data => data.password === data.password_confirmation, {
+  message: "Passwords must match",
+  path: ["password_confirmation"],
+});
+
+type TregisterFormSchema = z.infer<typeof registerFormSchema>;
 
 
 export default function Page() {
+  const form = useForm<TregisterFormSchema>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    },
+  });
 
-    const form = useForm();
+  const router = useRouter();
 
-    function onSubmit(){
-        
-    }
+  //useful to debug
+  //console.log(form.formState.errors)
+
+  function onSubmit(data: TregisterFormSchema) {
+    console.log(data);
+    api().get('/sanctum/csrf-cookie').then(() => {
+      api().post('/api/register', data).then(() => {
+        setLogInCookie();
+        router.push('/dashboard')
+      }).catch((error) => {
+        console.log(error)
+      })
+    })
+  }
 
   return (
     <main>
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="name" {...field}/>
-                            </FormControl>
-                            <FormDescription>
-                                Your real full name.
-                            </FormDescription>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit" >Submit</Button>
-            </form>
-        </Form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField  
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="name" {...field} />
+                </FormControl>
+                <FormDescription>Your real full name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="email@example.com" {...field} />
+                </FormControl>
+                <FormDescription>Your email address</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="password" type="password" {...field} />
+                </FormControl>
+                <FormDescription>Your password</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password_confirmation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password Confirmation</FormLabel>
+                <FormControl>
+                  <Input placeholder="password" type="password" {...field} />
+                </FormControl>
+                <FormDescription>Please confirm your password</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </main>
-  )
+  );
 }
