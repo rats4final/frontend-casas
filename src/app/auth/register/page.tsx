f3797f2637b4d2d060eb68eb2152526d6e8afc68
rats4final <rats4final@gmail.com>
2024-03-27 17:59:24 -0400

    register page formatted

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -74,7 +74,7 @@ export default function Page() {
                 <FormDescription>Your real full name.</FormDescription>
                 <FormMessage />
               </FormItem>
-            )}
+            )}  
           />
           <FormField
             control={form.control}
