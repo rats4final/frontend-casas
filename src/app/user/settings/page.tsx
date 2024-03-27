1c07cf7295d13b8d63fd5b8413eca5b45e060855
rats4final <rats4final@gmail.com>
2024-03-27 18:06:38 -0400

    added profile info and password reset components, also added a user state and and state to show those settings if the user was authed through google

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -2,13 +2,16 @@
 
 import Disable2FA from "@/components/auth/Disable2FA";
 import Enable2FA from "@/components/auth/Enable2FA";
+import PasswordReset from "@/components/user/password-reset";
+import ProfileInfo from "@/components/user/profile-info";
 import withAuth from "@/hocs/withAuth";
 import api from "@/lib/api";
 import { useEffect, useState } from "react";
 
 function Page() {
   const [twoFactorAuthEnable, setTwoFactorAuthEnable] = useState(false);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(false);
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
 
   useEffect(() => {
     api()
@@ -17,26 +20,43 @@ function Page() {
         console.log(response.data.data);
         setUser(response.data.data);
         setTwoFactorAuthEnable(!!response.data.data.has2FA);
+        if (response.data.data.google_id) {
+          setShowTwoFactor(false);
+        } else {
+          setShowTwoFactor(true);
+        }
       });
   }, []);
 
   return (
     <main>
       <h1>Account Settings</h1>
-      <p>Two Factor Auth is: {twoFactorAuthEnable ? "Enabled" : "Disabled"}</p>
-      {twoFactorAuthEnable ? (
+      {showTwoFactor ? (
+        <p>
+          Two Factor Auth is: {twoFactorAuthEnable ? "Enabled" : "Disabled"}
+        </p>
+      ) : (
+        ""
+      )}
+      {twoFactorAuthEnable && showTwoFactor ? (
         <Disable2FA
           onSuccess={() => {
             setTwoFactorAuthEnable(false);
           }}
         />
-      ) : (
+      ) : showTwoFactor ? (
         <Enable2FA
           onSuccess={() => {
             setTwoFactorAuthEnable(true);
           }}
         />
+      ) : (
+        ""
       )}
+
+    <ProfileInfo user={user}/>
+    <hr />
+    <PasswordReset/>
     </main>
   );
 }
