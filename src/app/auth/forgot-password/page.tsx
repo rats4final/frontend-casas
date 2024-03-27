32200287690408174e26e6147bc96ff6b4470515
rats4final <rats4final@gmail.com>
2024-03-27 18:07:06 -0400

    added forgot password schema and completed the form

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,21 +1,78 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import api from "@/lib/api";
 
 const forgotPasswordSchema = z.object({
-  email: z.string().email()
-})
+  email: z.string().email(),
+});
+
+type TforgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
 
 export default function Page() {
   const [emailSent, setEmailSent] = useState(false);
-  const form = useForm()
+  const form = useForm<TforgotPasswordSchema>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(data: TforgotPasswordSchema) {
+    api().get('/sanctum/csrf-cookie').then(() => {
+      api().post('/api/forgot-password', data).then(() => {
+        setEmailSent(true);
+      }).catch((errors) => {
+        console.log(errors);
+        setEmailSent(false);
+        form.reset();
+      })
+    })
+  }
 
   return (
-    <div>
-      <Input type="email"/>
-      <Button type="submit">Reset</Button>
-    </div>
-  )
+    <main>
+      {emailSent ? (
+        <div>
+          Instructions to reset your password have been sent to{" "}
+          {form.getValues("email")}
+        </div>
+      ) : (
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type="email" {...field} />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Reset</Button>
+            </form>
+          </Form>
+        </div>
+      )}
+    </main>
+  );
 }
