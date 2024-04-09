8ed476c29424540c8b9e3cbc035de36f4e7f70cb
rats4final <rats4final@gmail.com>
2024-04-08 22:40:32 -0400

    added some code to verify user is correctly set in the user store, also commented out the forcePageRefresh function since we don't need rely on that logic of reloading anymore

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,22 +2,29 @@
 import withAuth from "@/hocs/withAuth";
 import api from "@/lib/api";
 import { deleteLogInCookie, forcePageRefresh } from "@/lib/authCookies";
+import useUserStore from "@/lib/userStore";
 import { HomeIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 function Page() {
+  const {user} = useUserStore();
+  const router = useRouter();
   useEffect(() => {
-    async function getUserInfo() {
-      const data = await api().get("/api/user");
-      console.log(data.data);
-    }
-    getUserInfo();
+    // async function getUserInfo() {
+    //   const data = await api().get("/api/user");
+    //   console.log(data.data);
+    // }
+    // getUserInfo();
   }, []);
+
+  console.log(user.name)
 
   return (
     <main className="flex h-screen w-full items-center gap-4 px-2 py-4">
       <aside className="flex h-full w-64 flex-col gap-2">
-        <div className="h-1/4 w-full rounded-lg bg-blue-500">Inmobi</div>
+        <div className="h-1/4 w-full rounded-lg bg-blue-500">Inmobi {}</div>
         <div className="flex w-full items-center justify-start gap-2 rounded-lg bg-slate-300 p-4">
           <HomeIcon className="h-5 w-5" />
           Menu
@@ -28,7 +35,7 @@ function Page() {
         </div>
         <div className="flex w-full items-center justify-start gap-2 rounded-lg bg-slate-300 p-4">
           <HomeIcon className="h-5 w-5" />
-          Home
+          <Link href="/user/settings">Settings</Link>
         </div>
         <div className="mt-auto flex w-full items-center justify-start gap-2 rounded-lg bg-slate-300 p-4">
           <HomeIcon className="h-5 w-5" />
@@ -38,7 +45,10 @@ function Page() {
                 .post("/logout")
                 .then(() => {
                   deleteLogInCookie();
-                  forcePageRefresh();
+                  //forcePageRefresh();
+                  router.push('/auth/login')
+                  //maybe just send them back to login since we may not
+                  //need the page refresh?
                 });
             }}
           >
