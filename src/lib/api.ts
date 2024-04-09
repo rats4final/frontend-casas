75b455b6f12c5064cac5d2bdf387f7944b29c34b
rats4final <rats4final@gmail.com>
2024-04-08 22:33:38 -0400

    added interceptors back to the api instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
-"use client"
 import axios from "axios";
+import { deleteLogInCookie } from "./authCookies";
 export default function api() {
+
 
   const api = axios.create({
     baseURL: "http://localhost:8000",
@@ -11,15 +12,19 @@ export default function api() {
     withXSRFToken: true,
   });
 
-    // api.interceptors.response.use(
-    //   (response) => response,
-    //   (error) => {
-    //     if (error.response.status === 401) {
-    //       return Promise.reject();
-    //     }
-
-    //     return Promise.reject(error);
-    //   },
-    // );
+    api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          if(window){
+            deleteLogInCookie();
+            console.log(error)
+            window.location.replace('http://localhost:3000/auth/login');
+            return Promise.reject();
+          }
+        }
+        return Promise.reject(error);
+      },
+    );
   return api;
 }
