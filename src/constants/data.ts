cbea1e74583d699bae14da3112bd64ce8ef97635
rats4final <rats4final@gmail.com>
2024-05-03 14:54:07 -0400

    constantes del sidebar

---
+++ src/constants/data.ts
@@ -0,0 +1,40 @@
+import { NavItem } from "@/types";
+
+export const navItems: NavItem[] = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: "dashboard",
+    label: "Dashboard",
+  },
+  {
+    title: "User",
+    href: "/dashboard/user",
+    icon: "user",
+    label: "user",
+  },
+  {
+    title: "Property",
+    href: "/dashboard/properties",
+    icon: "home",
+    label: "property",
+  },
+  {
+    title: "Profile",
+    href: "/user/settings",
+    icon: "profile",
+    label: "profile",
+  },
+  {
+    title: "Kanban",
+    href: "/dashboard/kanban",
+    icon: "kanban",
+    label: "kanban",
+  },
+  {
+    title: "Login",
+    href: "/",
+    icon: "login",
+    label: "login",
+  },
+];
