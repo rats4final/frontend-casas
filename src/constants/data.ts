7e9fff8501e578730a08cf74012cff6c6e2dbb64
rats4final <rats4final@gmail.com>
2024-06-20 02:41:26 -0400

    added more entries to the navitems for dashboard sidebar as well as made it collapsible

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -8,33 +8,66 @@ export const navItems: NavItem[] = [
     label: "Dashboard",
   },
   {
-    title: "User",
-    href: "/dashboard/user",
-    icon: "user",
-    label: "user",
+    title: "Gestión de Propiedades",
+    isSection: true, // Indicamos que es una sección
+    children: [
+      {
+        title: "Usuarios",
+        href: "/dashboard/users",
+        icon: "user",
+        label: "users",
+      },
+      {
+        title: "Propiedades",
+        href: "/dashboard/properties",
+        icon: "home",
+        label: "properties",
+      },
+      {
+        title: "Casas",
+        href: "/dashboard/houses",
+        icon: "home",
+        label: "house",
+      },
+    ],
   },
   {
-    title: "Property",
-    href: "/dashboard/properties",
-    icon: "home",
-    label: "property",
+    title: "Gestión de Tareas",
+    isSection: true,
+    children: [
+      {
+        title: "Kanban",
+        href: "/dashboard/kanban",
+        icon: "kanban",
+        label: "kanban",
+      },
+      {
+        title: "Pruebas",
+        href: "/dashboard/pruebas",
+        icon: "home",
+        label: "pruebas",
+      },
+    ],
   },
   {
-    title: "Profile",
-    href: "/user/settings",
-    icon: "profile",
-    label: "profile",
-  },
-  {
-    title: "Kanban",
-    href: "/dashboard/kanban",
-    icon: "kanban",
-    label: "kanban",
-  },
-  {
-    title: "Login",
-    href: "/",
-    icon: "login",
-    label: "login",
+    title: "Otros",
+    isSection: true,
+    children: [
+      {
+        title: "Contratos",
+        href: "/dashboard/contracts",
+        label: "contracts",
+      },
+      {
+        title: "Respaldos",
+        href: "/dashboard/backups",
+        label: "backups",
+      },
+      {
+        title: "Registro de Actividades",
+        href: "/dashboard/logs",
+        label: "logs",
+      },
+    ],
   },
 ];
