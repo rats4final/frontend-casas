9e3594b98cc2af0a9ed4f1964fab13987a2800c9
rats4final <rats4final@gmail.com>
2024-06-20 02:56:07 -0400

    changed main page lang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,28 +7,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-8 p-10">
-      <div>
+      <div className="flex w-full items-center justify-evenly">
+        <Button asChild><Link href="/properties">Ver Propiedades</Link></Button>
         <ModeToggle/>
+        <Button asChild><Link href="/auth/login">Login</Link></Button>
       </div>
       {/* TODO::fix this by using an image with a fixed size, so that you don't use h-screen */}
       <div className="flex gap-3 w-full flex-col items-center justify-center md:bg-hero bg-hero-mobile bg-cover grow h-screen">
-        <p className="text-lg md:text-6xl">Discover Your New Home</p>
+        <p className="text-lg md:text-6xl">Descubre tu nuevo hogar</p>
         <p className="text-sm md:text-2xl">
-          Helping people find homes since 1992
+          Ayudando a la gente a encontrar la casa de sus suenios desde1992
         </p>
         <div className="w-2/3 mt-6 flex flex-col md:flex-row items-center gap-2">
           <Input type="search" placeholder="Search..."/>
-          <Button>Search</Button>
+          <Button>Buscar</Button>
         </div>
       </div>
       <div className="flex flex-col items-center gap-4">
-        <p className="text-xl md:text-4xl">The Most Rental Listings</p>
-        <p className="text-center md:text-lg">Choose from over 1000 apartments, houses, condos, and more!</p>
+        <p className="text-xl md:text-4xl">La Mayor Cantidad de Anuncios de Alquiler</p>
+        <p className="text-center md:text-lg">¡Elija entre más de 1000 apartamentos, casas, condominios y más!</p>
         <div className="flex flex-wrap md:flex-nowrap">
           <div className="flex flex-col gap-4 flex-wrap items-start justify-center pl-16 pr-14 py-6 bg-slate-100 dark:bg-slate-700">
-            <p>Rent Faster!</p>
-            <p>Browse the highest quality properties, apply online, sign your lease, and even take a 3D look!</p>
-            <Link href={"#"}>Find Out More</Link>
+            <p>Rente mas Rapido!</p>
+            <p>Explore propiedades de la más alta calidad, presente su solicitud en línea, firme su contrato de arrendamiento e incluso eche un vistazo en 3D.</p>
+            <Link href={"#"}>Mas Informacion</Link>
           </div>
           <Image
             className="grow"
